7675d99289a0180d3a8c1b852b1ccdff8b26e8b4
Simon Pythoud <simonpythoud@gmail.com>
2013-05-13 19:07:28 -0400

    Added production folder. Set environment to local

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.configure(function(){
     }));
     app.use(express.methodOverride());
     app.use(app.router);
-    app.use(express.static(__dirname + '/webapp/build/EgoMasks/production')); // '/webapp/build/EgoMasks/production'
+    app.use(express.static(__dirname + '/webapp'));
     app.set('view engine', 'jade');
 });
 
